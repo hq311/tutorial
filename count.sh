c20f884258a7dd44a44eac1307f327da6f049beb
hq311 <haiqi2009@gmail.com>
2016-01-24 14:07:49 -0500

    elaborated on comment in count.sh

--- count.sh
+++ count.sh
@@ -1,9 +1,10 @@
 # Display the name of each data file and count
 # the number of distinct occurences of each species
-# in that file
+# in that file. Results are displayed in alphabetical order
+# by species name
 
 for filename in data/*.txt
 do
 	echo $filename
-	grep -v Species $data | cut -d, -f 2 |sort |uniq -c
+	grep -v Species $filename | cut -d, -f 2 |sort |uniq -c
 done
